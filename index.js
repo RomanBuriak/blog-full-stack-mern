1610b8ae6e48a0e17bce74df6adb7fd201387742
RomanBuriak <46033015+RomanBuriak@users.noreply.github.com>
2024-08-21 21:19:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,134 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
+import bcrypt from "bcrypt";
+import checkAuth from "./utils/checkAuth.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://roman:vBgprUrmDiwAsIRb@cluster0.zvm6s.mongodb.net/blogS?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Glory to Ukraine!");
+});
+
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secretsecret",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не вдалось зареєструватись",
+    });
+  }
+});
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Користувач не знайдений",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Невірний логін чи пароль",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secretsecret",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не вдалось авторизуватись",
+    });
+  }
+});
+
+app.get("/auth/me", checkAuth, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Користувач не знайдений",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json({ ...userData });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не вдалось ",
+    });
+  }
+});
+
+app.listen(1234, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
